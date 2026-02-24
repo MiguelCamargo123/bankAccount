68f76d64a5b2ee38ff48b1be9cb0fb41def1490d
Miguel Camargo <omiguelcamargo@gmail.com>
2026-02-24 10:12:50 -0300

    Histórico adicionado e função de ver histórico adicionada

--- bankAccount.py
+++ bankAccount.py
@@ -2,13 +2,20 @@ class contaBancaria:
     def __init__(self, titular, saldo):
         self.titular = titular
         self.__saldo = saldo
+        self.historico = []
 
     def depositar(self):
         valorDepositar = int(input('Digite um valor: '))
         if valorDepositar > 0:
             self.__saldo += valorDepositar
+            self.historico.append({
+                'Tipo': 'depósito',
+                'Valor': valorDepositar,
+                'Saldo Após': self.__saldo
+            })
         else:
             print('Digite um valor acima de zero')
+
 
     def sacar(self):
         valorSacar = int(input('Digite um valor para sacar: '))
@@ -20,9 +27,19 @@ class contaBancaria:
         else:
             self.__saldo -= valorSacar
             print('Valor sacado com sucesso')
+            self.historico.append({
+                'Tipo': 'saque',
+                'Valor': valorSacar,
+                'Saldo Após': self.__saldo
+            })
+
 
     def verSaldo(self):
         print(f'O seu saldo atual é de {self.__saldo}')
+
+    def verHistorico(self):
+        for transacao in self.historico:
+            print(transacao)
 
 titular = input('Digite seu nome: ')
 

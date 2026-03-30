66d910042c19522ff55dd1baa1d84dc5057af5eb
Miguel Camargo <omiguelcamargo@gmail.com>
2026-03-30 11:24:00 -0300

    v1.3.0 concluida (usei tipagem nela)

--- bankAccount.py
+++ bankAccount.py
@@ -13,19 +13,19 @@ class Conta:
         self.__saldo: float = 0.0
         self.__carregar()
 
-    def __carregar(self):
+    def __carregar(self) -> None:
         try:
             with open('historico.json', 'r', encoding='utf-8') as f:
                 self.historico = json.load(f)
         except FileNotFoundError:
             self.historico = []
 
-    def __salvar(self):
+    def __salvar(self) -> None:
         with open('historico.json', 'w', encoding='utf-8') as h:
             json.dump(self.historico, h, ensure_ascii=False, indent=4)
 
-    def depositar(self, valor):
-        if valor > 0:
+    def depositar(self, valor: float) -> float:
+        if valor > 0.0:
             self.__saldo += valor
             self.historico.append(
                 {'Tipo': 'depósito', 'Valor': valor, 'Saldo Após': self.__saldo}
@@ -34,8 +34,8 @@ class Conta:
             print('Digite um valor acima de zero')
         self.__salvar()
 
-    def sacar(self, valor):
-        if valor <= 0:
+    def sacar(self, valor: float):
+        if valor <= 0.0:
             print('Digite um valor maior que zero para poder sacar')
         elif valor >= self.__saldo:
             print('Digite um valor menor que seu saldo')
@@ -47,20 +47,20 @@ class Conta:
             )
         self.__salvar()
 
-    def verSaldo(self):
+    def verSaldo(self) -> None:
         print(f'O seu saldo atual é de {self.__saldo}')
 
-    def verHistorico(self):
+    def verHistorico(self) -> None:
         for transacao in self.historico:
             print(transacao)
 
 
 class Banco:
-    def __init__(self):
-        self.contas = []
+    def __init__(self) -> None:
+        self.contas: list[Conta] = []
         self.__carregar_json_banco()
 
-    def __carregar_json_banco(self):
+    def __carregar_json_banco(self) -> None:
         try:
             with open('contas.json', 'r', encoding='utf-8') as f:
                 dados = json.load(f)
@@ -72,8 +72,8 @@ class Banco:
         except FileNotFoundError:
             self.contas = []
 
-    def __salvar_json_banco(self):
-        dados = []
+    def __salvar_json_banco(self) -> None:
+        dados: list[dict[str, Any]] = []
         for conta in self.contas:
             dados.append(
                 {
@@ -86,12 +86,14 @@ class Banco:
         with open('contas.json', 'w', encoding='utf-8') as c:
             json.dump(dados, c, ensure_ascii=False, indent=4)
 
-    def registrar_pessoa_banco(self, titular, senha):
+    def registrar_pessoa_banco(self, titular: str, senha: str) -> None:
         pessoa = Conta(titular, senha)
         self.contas.append(pessoa)
         self.__salvar_json_banco()
 
-    def verificar_se_conta_certa(self, nome_titular, senha_digitada):
+    def verificar_se_conta_certa(
+        self, nome_titular: str, senha_digitada: str
+    ) -> tuple[bool, Conta | None]:
         for conta in self.contas:
             if conta.titular == nome_titular:
                 if hashlib.sha256(senha_digitada.encode()).hexdigest() == conta.senha:
@@ -101,7 +103,7 @@ class Banco:
         return False, None
 
 
-def main():
+def main() -> None:
     banco = Banco()
 
     try:
@@ -123,9 +125,44 @@ def main():
             match fazer_oque:
                 case 'D':
                     try:
-                        valor = int(input('Digite um valor para depositar: '))
+                        valor = float(input('Digite um valor para depositar: '))
                         conta.depositar(valor)
+                        print('Valor depositado com sucesso!!!')
                     except ValueError:
                         print(
                             'Digite um valor (um número) para depositar na sua conta!!!'
                         )
+
+                case 'S':
+                    try:
+                        saque = float(input('Digite um valor para sacar: '))
+                        conta.sacar(saque)
+                        print('Valor sacado com sucesso!!!')
+                    except ValueError:
+                        print('Digite um valor (um número) para sacar!!!!')
+
+                case 'V':
+                    conta.verSaldo()
+
+                case 'H':
+                    conta.verHistorico()
+
+            deseja_sair = ('Voce deseja sair do banco? (S/N) ').upper()
+            if deseja_sair == 'S':
+                break
+
+    else:
+        pergunta = (
+            'Não encontramos essa conta, se você não tem uma conta, digite C, caso contrario, digite S '
+        ).upper()
+        if pergunta == 'C':
+            try:
+                titular = str('Digite o seu nome para criarmos a conta: ')
+                senha2 = str('Digite sua senha para criarmos a conta: ')
+                banco.registrar_pessoa_banco(titular, senha2)
+            except ValueError:
+                print('Digite o seu nome (um texto), não um número!!!!')
+
+
+if __name__ == '__main__':
+    main()
